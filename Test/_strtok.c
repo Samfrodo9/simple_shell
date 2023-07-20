894dc052f6da3914d4185de3f5a92421b04c3a05
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-07-20 09:39:54 +0000

    Test folder

---
+++ Test/_strtok.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * _strtok - strtok function
+ *
+ * Return: an array of strings
+ */
+
+char **_strtok(char *str, char *delim)
+{
+	char **strings = NULL;
+	char *temp = NULL;
+
+	int i = 0;
+
+	strings = malloc(sizeof(char *) * (_strlen(str) + 1));
+
+	if (!strings)
+		exit(EXIT_FAILURE);
+	
+	temp = strtok(str, delim);
+
+	while(temp != NULL)
+	{
+		strings[i] = malloc(sizeof(char) * (_strlen(temp) + 1));
+		_strcpy(strings[i], temp); /* Handle Null terminating character */
+		i++;
+		temp = strtok(NULL, delim);
+	}
+
+	strings[i] = NULL;
+	return (strings);
+}
---
+++ Test/shell.h
@@ -0,0 +1,29 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+char *_strstr(char *haystack, char *needle);
+void prompt(void);
+int _putchar(char c);
+void interactive(void);
+void non_interactive(void);
+char *_getline(void);
+char **_strtok(char *str, char *delim);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void _puts(char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void add_path(char *arg, char **argv);
+int _execute(char *path, char **argv);
+int check(char *str, char**buffer);
+
+#endif
